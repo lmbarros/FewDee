c63fcd66892eb53e5bcc3555cb130c0b335cd750
Leandro Motta Barros <lmb@stackedboxes.org>
2012-11-04 12:53:58 -0200

    Organized examples. Added a new one: states_simple.

--- Makefile
+++ Makefile
@@ -5,19 +5,30 @@
 DMDCOMMONFLAGS=-Isrc
 DMDFLAGS=-unittest $(DMDCOMMONFLAGS)
 # DMDFLAGS=-O -inline $(DMDCOMMONFLAGS)
-# DMDFLAGS=-debug $(DMDCOMMONFLAGS)
+# DMDFLAGS=-debug -gc $(DMDCOMMONFLAGS)
 
 DMDLINKFLAGS=twodee.a -L-lallegro -L-lallegro_image -L-lallegro_font -L-lallegro_ttf
 
-all: twodee.a twodeedemo
+# Implicit rule to build an example
+%.example: examples/%.d
+	dmd $(DMDFLAGS) $(DMDLINKFLAGS) -of$@ $< twodee.a
+
+
+# All
+all: twodee.a twodeedemo.example states_simple.example
+
 
 # The library
 twodee.a: src/twodee/*
 	dmd $(DMDFLAGS) -lib -oftwodee.a src/allegro5/*.d src/allegro5/internal/*.d \
 	   src/twodee/*.d src/twodee/sg/*.d
 
-twodeedemo: examples/twodeedemo.d twodee.a
-	dmd $(DMDFLAGS) $(DMDLINKFLAGS) -oftwodeedemo examples/twodeedemo.d twodee.a
 
+# The examples
+twodeedemo.example: examples/twodeedemo.d
+states_simple.example: examples/states_simple.d
+
+
+# Clean
 clean:
-	rm -f twodee.a twodeedemo twodeedemo.o
+	rm -f twodee.a *.o *.example
---
+++ examples/states_simple.d
@@ -0,0 +1,119 @@
+/**
+ * A simple example showing the use of game states. We create a few states and
+ * allow the user to change between them by pressing some keys.
+ *
+ * Authors: Leandro Motta Barros
+ */
+
+import allegro5.allegro;
+import allegro5.allegro_font;
+import allegro5.allegro_ttf;
+
+import twodee.engine;
+import twodee.event;
+import twodee.game_state;
+import twodee.state_manager;
+
+import std.exception;
+
+
+// All states in this example are very similar to each other. To avoid
+// duplicated code, this class contains everything shared among our game states.
+class BaseState: GameState
+{
+   this()
+   {
+      font_ = al_load_ttf_font("data/bluehigl.ttf", 30, 0);
+      enforce(font_ !is null);
+   }
+
+   ~this()
+   {
+      al_destroy_font(font_);
+   }
+
+   protected void drawText(string text, float x, float y)
+   {
+      al_draw_text(font_, al_map_rgb(255, 255, 255), x, y, ALLEGRO_ALIGN_LEFT,
+                   text.ptr);
+   }
+
+   protected ALLEGRO_FONT* font_;
+}
+
+
+class InitialState: BaseState
+{
+   this()
+   {
+      addEventCallback(ALLEGRO_EVENT_KEY_DOWN,
+                       delegate(in ref ALLEGRO_EVENT event)
+                       {
+                          if (event.keyboard.keycode == ALLEGRO_KEY_SPACE)
+                             pushState(new StateA());
+                          else if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+                             popState();
+                       });
+   }
+
+   public void onDraw()
+   {
+      al_clear_to_color(al_map_rgb(10, 10, 50));
+      drawText("Initial State", 30, 30);
+      drawText("Press space to push a State A", 50, 60);
+      drawText("Press ESC to quit", 50, 90);
+   }
+};
+
+
+class StateA: BaseState
+{
+   this()
+   {
+      addEventCallback(ALLEGRO_EVENT_KEY_DOWN,
+                       delegate(in ref ALLEGRO_EVENT event)
+                       {
+                          if (event.keyboard.keycode == ALLEGRO_KEY_B)
+                             replaceState(new StateB());
+                          else if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+                             popState();
+                       });
+   }
+
+   public void onDraw()
+   {
+      al_clear_to_color(al_map_rgb(50, 10, 10));
+      drawText("State A", 30, 30);
+      drawText("Press ESC to go back to the initial state", 50, 60);
+      drawText("Press \"B\" to replace this state with State B", 50, 90);
+   }
+}
+
+class StateB: BaseState
+{
+   this()
+   {
+      addEventCallback(ALLEGRO_EVENT_KEY_DOWN,
+                       delegate(in ref ALLEGRO_EVENT event)
+                       {
+                          if (event.keyboard.keycode == ALLEGRO_KEY_A)
+                             replaceState(new StateA());
+                          else if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE)
+                             popState();
+                       });
+   }
+
+   public void onDraw()
+   {
+      al_clear_to_color(al_map_rgb(10, 50, 10));
+      drawText("State B", 30, 30);
+      drawText("Press ESC to go back to the initial state", 50, 60);
+      drawText("Press \"A\" to replace this state with State A", 50, 90);
+   }
+}
+
+void main()
+{
+   auto engine = twodee.engine.Engine(640, 480);
+   engine.run(new InitialState());
+}
