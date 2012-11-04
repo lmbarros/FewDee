9cee1e726369654a3f451c5a2ad4fe8062b61810
Leandro Motta Barros <lmb@stackedboxes.org>
2012-11-04 18:52:46 -0200

    Changes to documentation. Mostly stylistic.

--- src/twodee/sg/drawable.d
+++ src/twodee/sg/drawable.d
@@ -13,9 +13,7 @@ import twodee.sg.node_visitor;
 /// A scene graph node that can draw itself.
 class Drawable: Node
 {
-   /**
-    * Draws the Drawable to Allegro's "current bitmap".
-    */
+   /// Draws the Drawable to Allegro's "current bitmap".
    abstract public void draw();
 
    /// Accepts a NodeVisitor. The Visitor pattern, you know.
@@ -24,16 +22,17 @@ class Drawable: Node
       visitor.visit(this);
    }
 
-   /// The Drawable's "z-order".
+   /// Gets the Drawable's "z-order".
    public @property float z() const { return z_; }
 
-   /// The Drawable's "z-order".
+   /// Sets the Drawable's "z-order".
    public @property void z(float newZ) { z_ = newZ; }
 
    /*
-    * The Drawable's "z-order". Drawables with lower z-order are drawn first. In
-    * other words, Drawables with higher "z" are drawn over Drawables with lower
-    * "z". Drawables with the same "z-order" can be drawn in any order.
+    * The Drawable's "z-order". When using the standard means, Drawables with
+    * lower z-order are drawn first. In other words, Drawables with higher "z"
+    * are drawn over Drawables with lower "z". Drawables with the same "z-order"
+    * can be drawn in any order.
     */
    private float z_ = 0.0;
 }
