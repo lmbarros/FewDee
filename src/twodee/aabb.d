49b362ed41f9e907a0dbe08389fc5f6739d23e19
Leandro Motta Barros <lmb@stackedboxes.org>
2012-11-07 22:36:34 -0200

    Added EmptyAABB.
    Also moved the unit tests to some place where they are really
    executed: As of DMD 2.060, it seems that "global" unit tests (when
    defined in a library, at least), are not run when the program (linking
    with the library) runs.

--- src/twodee/aabb.d
+++ src/twodee/aabb.d
@@ -62,49 +62,60 @@ struct AABB
 
    /// The AABB's right side, measured from the left of the screen.
    public float right;
+
+   // Tests AABB.contains()
+   unittest
+   {
+      auto aabb = AABB(10.0, 30.0, 5.0, 40.0);
+
+      assert(aabb.contains(5, 12));
+      assert(!aabb.contains(0.0, 0.0));
+      assert(!aabb.contains(50, 12));
+      assert(aabb.contains(35.0, 29.0));
+      assert(aabb.contains(10.0, 10.0)); // point along top segment
+      assert(!aabb.contains(10.0, 30.0)); // point along bottom segment
+      assert(aabb.contains(5.0, 20.0)); // point along left segment
+      assert(!aabb.contains(5.0, 40.0)); // point along right segment
+
+      // EmptyAABB doesn't contain any point
+      assert(!EmptyAABB.contains(0.0, 0.0));
+   }
+
+   // Tests AABB.unionWith()
+   unittest
+   {
+      auto aabb1 = AABB(-2, 10, 3, 8);
+      const aabb2 = AABB(-1, 11, 4, 8);
+
+      aabb1.unionWith(aabb2);
+      assert(aabb1.top == -2);
+      assert(aabb1.bottom == 11);
+      assert(aabb1.left == 3);
+      assert(aabb1.right == 8);
+
+      const aabb3 = AABB(3, 4, 5, 7);
+      aabb1.unionWith(aabb3);
+      assert(aabb1.top == -2);
+      assert(aabb1.bottom == 11);
+      assert(aabb1.left == 3);
+      assert(aabb1.right == 8);
+
+      const aabb4 = AABB(-100, 100, -100, 100);
+      aabb1.unionWith(aabb4);
+      assert(aabb1.top == -100);
+      assert(aabb1.bottom == 100);
+      assert(aabb1.left == -100);
+      assert(aabb1.right == 100);
+
+      // Anything.unionWith(EmptyAABB) shall be equal to Anything
+      aabb1.unionWith(EmptyAABB);
+      assert(aabb1.top == -100);
+      assert(aabb1.bottom == 100);
+      assert(aabb1.left == -100);
+      assert(aabb1.right == 100);
+   }
 }
 
 
-// Tests AABB.contains()
-unittest
-{
-   auto aabb = AABB(10.0, 30.0, 5.0, 40.0);
-
-   assert(aabb.contains(5, 12));
-   assert(!aabb.contains(0.0, 0.0));
-   assert(!aabb.contains(50, 12));
-   assert(aabb.contains(35.0, 29.0));
-   assert(aabb.contains(10.0, 10.0)); // point along top segment
-   assert(!aabb.contains(10.0, 30.0)); // point along bottom segment
-   assert(aabb.contains(5.0, 20.0)); // point along left segment
-   assert(!aabb.contains(5.0, 40.0)); // point along right segment
-}
-
-
-// Tests AABB.unionWith()
-unittest
-{
-   auto aabb1 = AABB(-2, 10, 3, 8);
-   const aabb2 = AABB(-1, 11, 4, 8);
-
-   aabb1.unionWith(aabb2);
-   assert(aabb1.top == -2);
-   assert(aabb1.bottom == 11);
-   assert(aabb1.left == 3);
-   assert(aabb1.right == 8);
-
-   const aabb3 = AABB(3, 4, 5, 7);
-   aabb1.unionWith(aabb3);
-   assert(aabb1.top == -2);
-   assert(aabb1.bottom == 11);
-   assert(aabb1.left == 3);
-   assert(aabb1.right == 8);
-
-   const aabb4 = AABB(-100, 100, -100, 100);
-   aabb1.unionWith(aabb4);
-   assert(aabb1.top == -100);
-   assert(aabb1.bottom == 100);
-   assert(aabb1.left == -100);
-   assert(aabb1.right == 100);
-}
-
+/// An empty bounding box.
+immutable EmptyAABB = AABB(float.nan, float.nan, float.nan, float.nan);
