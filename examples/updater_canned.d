2ebd6b4966360ea3fcb1983f3ad457a624a1428d
Leandro Motta Barros <lmb@stackedboxes.org>
2012-12-08 15:14:51 -0200

    Finished implementing the canned updaters; completed example.

--- examples/updater_canned.d
+++ examples/updater_canned.d
@@ -6,6 +6,7 @@
 
 import std.exception;
 import std.functional;
+import std.math;
 import fewdee.all;
 
 
@@ -17,12 +18,14 @@ class TheState: GameState
    this()
    {
       // Initialize stuff
-      bitmap_ = al_load_bitmap("data/small_cloud.png");
+      bitmap_ = al_load_bitmap("data/white_circle.png");
       enforce(bitmap_ !is null);
 
       sprite_ = new Sprite(64, 64, 32, 32);
       enforce(sprite_ !is null);
       sprite_.addBitmap(bitmap_);
+      sprite_.x = WIDTH/2.0;
+      sprite_.y = HEIGHT/2.0;
 
       updater_ = new Updater();
       enforce(updater_ !is null);
@@ -40,30 +43,184 @@ class TheState: GameState
                                 break;
                              }
 
+                             // Position updaters
                              case ALLEGRO_KEY_1:
                              {
                                 updater_.addPositionUpdater(
-                                   sprite_, 500.0, 300.0, 5.0,
+                                   sprite_, 30.0, 30.0, 3.5,
                                    &MakeLinearInterpolator);
                                 break;
                              }
 
-                             case ALLEGRO_KEY_2:
+                             case ALLEGRO_KEY_Q:
                              {
                                 updater_.addPositionUpdater(
-                                   sprite_, 110, 240, 5,
+                                   sprite_, 600, 40, 3.5,
+                                   &MakeQuadraticInOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_A:
+                             {
+                                updater_.addPositionUpdater(
+                                   sprite_, 530, 410, 6.0,
+                                   MakeGenericElasticOutInterpolatorMaker());
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_Z:
+                             {
+                                updater_.addPositionUpdater(
+                                   sprite_, 55, 430, 2.0,
+                                   &MakeBounceInInterpolator);
+                                break;
+                             }
+
+                             // Alpha updaters
+                             case ALLEGRO_KEY_2:
+                             {
+                                updater_.addAlphaUpdater(
+                                   sprite_, 1.0, 2.0,
+                                   &MakeQuadraticOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_W:
+                             {
+                                updater_.addAlphaUpdater(
+                                   sprite_, 0.66, 2.0,
+                                   &MakeCircleInInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_S:
+                             {
+                                updater_.addAlphaUpdater(
+                                   sprite_, 0.33, 2.0,
+                                   &MakeQuinticOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_X:
+                             {
+                                updater_.addAlphaUpdater(
+                                   sprite_, 0.0, 2.0,
+                                   &MakeExponentialInOutInterpolator);
+                                break;
+                             }
+
+                             // Color updaters
+                             case ALLEGRO_KEY_3:
+                             {
+                                immutable color =
+                                   al_map_rgba_f(1.0, 1.0, 1.0, 1.0);
+
+                                updater_.addColorUpdater(
+                                   sprite_, color, 2.0,
+                                   &MakeCubicInInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_E:
+                             {
+                                immutable color =
+                                   al_map_rgba_f(1.0, 0.0, 0.0, 0.1);
+
+                                updater_.addColorUpdater(
+                                   sprite_, color, 2.0,
+                                   &MakeSineOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_D:
+                             {
+                                immutable color =
+                                   al_map_rgba_f(0.2, 0.2, 1.0, 1.0);
+
+                                updater_.addColorUpdater(
+                                   sprite_, color, 2.0,
                                    &MakeQuarticInOutInterpolator);
                                 break;
                              }
 
-                             case ALLEGRO_KEY_3:
+                             case ALLEGRO_KEY_C:
                              {
-                                updater_.addPositionUpdater(
-                                   sprite_, 600, 60, 5,
+                                immutable color =
+                                   al_map_rgba_f(0.1, 0.9, 0.3, 0.9);
+
+                                updater_.addColorUpdater(
+                                   sprite_, color, 2.0,
+                                   &MakeExponentialInInterpolator);
+                                break;
+                             }
+
+                             // Scale updaters
+                             case ALLEGRO_KEY_4:
+                             {
+                                updater_.addScaleUpdater(
+                                   sprite_, 1.0, 1.0, 2.0,
+                                   &MakeBounceOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_R:
+                             {
+                                updater_.addScaleUpdater(
+                                   sprite_, 0.5, 0.5, 2.0,
+                                   &MakeCubicInOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_F:
+                             {
+                                updater_.addScaleUpdater(
+                                   sprite_, 1.7, -0.8, 2.0,
+                                   &MakeQuarticInInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_V:
+                             {
+                                updater_.addScaleUpdater(
+                                   sprite_, 1.8, 1.8, 2.0,
+                                   MakeGenericBackInInterpolatorMaker());
+                                break;
+                             }
+
+                             // Rotation updaters
+                             case ALLEGRO_KEY_5:
+                             {
+                                updater_.addRotationUpdater(
+                                   sprite_, 0.0, 2.0,
+                                   MakeGenericBackInOutInterpolatorMaker());
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_T:
+                             {
+                                updater_.addRotationUpdater(
+                                   sprite_, PI, 2.0,
                                    MakeGenericElasticInOutInterpolatorMaker());
                                 break;
                              }
 
+                             case ALLEGRO_KEY_G:
+                             {
+                                updater_.addRotationUpdater(
+                                   sprite_, -PI, 2.0,
+                                   &MakeCircleInOutInterpolator);
+                                break;
+                             }
+
+                             case ALLEGRO_KEY_B:
+                             {
+                                updater_.addRotationUpdater(
+                                   sprite_, 5*PI, 2.0,
+                                   &MakeSineOutInterpolator);
+                                break;
+                             }
+
+                             // Default
                              default:
                                 break; // do nothing
                           }
@@ -77,7 +234,7 @@ class TheState: GameState
 
    public override void onDraw()
    {
-      al_clear_to_color(al_map_rgb_f(0.3, 0.4, 1.0));
+      al_clear_to_color(al_map_rgb_f(0.1, 0.1, 0.1));
       sprite_.draw();
    }
 
--- src/fewdee/canned_updaters.d
+++ src/fewdee/canned_updaters.d
@@ -1,18 +1,45 @@
 /**
  * A set of handy, ready-to-use updaters.
  *
+ * These updaters naïve in a certain sense. They simply do their job without
+ * knowing anything about the rest of the world. If you add, say, two different
+ * position updaters acting upon the same object, they will "fight" with each
+ * other and the object will probably jump between unrelated positions.
+ *
+ * On the other hand, using requires very little effort, and therefore they are
+ * an excellent way to make your graphics and interfaces more "lively".
+ *
  * Authors: Leandro Motta Barros
  */
 
 module fewdee.canned_updaters;
 
+import allegro5.allegro;
+import fewdee.colorable;
 import fewdee.interpolators;
 import fewdee.positionable;
+import fewdee.rotatable;
+import fewdee.scalable;
 import fewdee.updater;
 
 
-// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-// xxxxxxxxxxxx Docs!
+/**
+ * Adds to a given Updater, an updater function that will change the position of
+ * a given object. D's uniform function call syntax allows to call this as if it
+ * was a method of Updater.
+ *
+ * Parameters:
+ *    updater = The Updater to which the updater function will be added.
+ *    target = The object whose position will be updated.
+ *    destX = The x coordinate of the destination position.
+ *    destY = The y coordinate of the destination position.
+ *    duration = The time, in seconds, it will take to go from the current to
+ *       the target position.
+ *    maker = A function that will be used to create the necessary
+ *       interpolators. Must have the same signature as
+ *       twodee.interpolators.GenericInterpolatorMakerDelegate_t (though it
+ *       doesn't have to be of this type).
+ */
 public void addPositionUpdater(T)(Updater updater, Positionable target,
                                   float destX, float destY, double duration,
                                   T maker)
@@ -33,3 +60,177 @@ public void addPositionUpdater(T)(Updater updater, Positionable target,
 }
 
 
+/**
+ * Adds to a given Updater, an updater function that will change the alpha
+ * (opacity) of a given object. D's uniform function call syntax allows to call
+ * this as if it was a method of Updater.
+ *
+ * Notice that this works just like a color interpolator -- except that it
+ * touches only the alpha channel. A color interpolator and an alpha
+ * interpolator would "fight" with each other if active at the same time on the
+ * same target.
+ *
+ * Parameters:
+ *    updater = The Updater to which the updater function will be added.
+ *    target = The object whose alpha will be updated.
+ *    destAlpha = The desired target alpha.
+ *    duration = The time, in seconds, it will take to go from the current to
+ *       the target alpha.
+ *    maker = A function that will be used to create the necessary
+ *       interpolators. Must have the same signature as
+ *       twodee.interpolators.GenericInterpolatorMakerDelegate_t (though it
+ *       doesn't have to be of this type).
+ */
+public void addAlphaUpdater(T)(Updater updater, Colorable target,
+                               float destAlpha, double duration, T maker)
+   if (__traits(isFloating, maker(0.0, 0.0, 1.0)(0.0)))
+{
+   double t = 0.0;
+
+   float ir, ig, ib, ia;
+   al_unmap_rgba_f(target.color, &ir, &ig, &ib, &ia);
+
+   auto alphaInterpolator = maker(ia, destAlpha, duration);
+
+   updater.add(delegate(dt)
+               {
+                  t += dt;
+
+                  float r, g, b, a;
+                  al_unmap_rgba_f(target.color, &r, &g, &b, &a);
+
+                  immutable newColor =
+                     al_map_rgba_f(r, g, b, alphaInterpolator(t));
+
+                  target.color = newColor;
+
+                  return t < duration;
+               });
+}
+
+
+/**
+ * Adds to a given Updater, an updater function that will change the color of a
+ * given object. D's uniform function call syntax allows to call this as if it
+ * was a method of Updater.
+ *
+ * A color updater can change the alpha (opacity) of an object, but if you want
+ * to use only the alpha channel, using an alpha interpolator is simpler.
+ *
+ * Parameters:
+ *    updater = The Updater to which the updater function will be added.
+ *    target = The object whose color will be updated.
+ *    destColor = The desired target color.
+ *    duration = The time, in seconds, it will take to go from the current to
+ *       the target color.
+ *    maker = A function that will be used to create the necessary
+ *       interpolators. Must have the same signature as
+ *       twodee.interpolators.GenericInterpolatorMakerDelegate_t (though it
+ *       doesn't have to be of this type).
+ *
+ * See_Also: addAlphaUpdater
+ */
+public void addColorUpdater(T)(Updater updater, Colorable target,
+                               in ref ALLEGRO_COLOR destColor, double duration,
+                               T maker)
+   if (__traits(isFloating, maker(0.0, 0.0, 1.0)(0.0)))
+{
+   double t = 0.0;
+
+   float ir, ig, ib, ia;
+   al_unmap_rgba_f(target.color, &ir, &ig, &ib, &ia);
+
+   float fr, fg, fb, fa;
+   al_unmap_rgba_f(destColor, &fr, &fg, &fb, &fa);
+
+   auto rInterpolator = maker(ir, fr, duration);
+   auto gInterpolator = maker(ig, fg, duration);
+   auto bInterpolator = maker(ib, fb, duration);
+   auto aInterpolator = maker(ia, fa, duration);
+
+   updater.add(delegate(dt)
+               {
+                  t += dt;
+                  immutable newColor =
+                     al_map_rgba_f(
+                        rInterpolator(t),
+                        gInterpolator(t),
+                        bInterpolator(t),
+                        aInterpolator(t));
+
+                  target.color = newColor;
+
+                  return t < duration;
+               });
+}
+
+
+/**
+ * Adds to a given Updater, an updater function that will change the scale of a
+ * given object. D's uniform function call syntax allows to call this as if it
+ * was a method of Updater.
+ *
+ * Parameters:
+ *    updater = The Updater to which the updater function will be added.
+ *    target = The object whose scale will be updated.
+ *    destScaleX = The desired target scale along the x axis.
+ *    destScaleY = The desired target scale along the y axis.
+ *    duration = The time, in seconds, it will take to go from the current to
+ *       the target scale.
+ *    maker = A function that will be used to create the necessary
+ *       interpolators. Must have the same signature as
+ *       twodee.interpolators.GenericInterpolatorMakerDelegate_t (though it
+ *       doesn't have to be of this type).
+ */
+public void addScaleUpdater(T)(Updater updater, Scalable target,
+                               float destScaleX, float destScaleY,
+                               double duration, T maker)
+   if (__traits(isFloating, maker(0.0, 0.0, 1.0)(0.0)))
+{
+   double t = 0.0;
+
+   auto xScaleInterpolator = maker(target.scaleX, destScaleX, duration);
+   auto yScaleInterpolator = maker(target.scaleY, destScaleY, duration);
+
+   updater.add(delegate(dt)
+               {
+                  t += dt;
+                  target.scaleX = xScaleInterpolator(t);
+                  target.scaleY = yScaleInterpolator(t);
+                  return t < duration;
+               });
+}
+
+
+/**
+ * Adds to a given Updater, an updater function that will change the rotation of
+ * a given object. D's uniform function call syntax allows to call this as if it
+ * was a method of Updater.
+ *
+ * Parameters:
+ *    updater = The Updater to which the updater function will be added.
+ *    target = The object whose rotation will be updated.
+ *    destAlpha = The desired target rotation, in radians.
+ *    duration = The time, in seconds, it will take to go from the current to
+ *       the target scale.
+ *    maker = A function that will be used to create the necessary
+ *       interpolators. Must have the same signature as
+ *       twodee.interpolators.GenericInterpolatorMakerDelegate_t (though it
+ *       doesn't have to be of this type).
+ */
+public void addRotationUpdater(T)(Updater updater, Rotatable target,
+                                  float destRotation, double duration,
+                                  T maker)
+   if (__traits(isFloating, maker(0.0, 0.0, 1.0)(0.0)))
+{
+   double t = 0.0;
+
+   auto rotInterpolator = maker(target.rotation, destRotation, duration);
+
+   updater.add(delegate(dt)
+               {
+                  t += dt;
+                  target.rotation = rotInterpolator(t);
+                  return t < duration;
+               });
+}
