a8dff6ba4287750d742b2241ad573f9017e28d82
Leandro Motta Barros <lmb@stackedboxes.org>
2013-07-14 01:02:20 -0300

    Updated to the latest DAllegro revision, which works with Allegro 5.0.10.

--- src/allegro5/allegro_ttf.d
+++ src/allegro5/allegro_ttf.d
@@ -11,8 +11,18 @@ import allegro5.allegro_font;
 
 extern (C)
 {
+	enum
+	{
+		ALLEGRO_TTF_NO_KERNING   = 1,
+		ALLEGRO_TTF_MONOCHROME   = 2,
+		ALLEGRO_TTF_NO_AUTOHINT  = 4
+	}
+
 	ALLEGRO_FONT* al_load_ttf_font(in char* filename, int size, int flags);
-	ALLEGRO_FONT* al_load_ttf_font_entry(ALLEGRO_FILE* file, in char* filename, int size, int flags);
+	ALLEGRO_FONT* al_load_ttf_font_f(ALLEGRO_FILE* file, in char* filename, int size, int flags);
+	ALLEGRO_FONT* al_load_ttf_font_stretch(in char* filename, int w, int h, int flags);
+	ALLEGRO_FONT* al_load_ttf_font_stretch_f(ALLEGRO_FILE *file, in char* filename, int w, int h, int flags);
+
 	bool al_init_ttf_addon();
 	void al_shutdown_ttf_addon();
 	uint al_get_allegro_ttf_version();
--- src/allegro5/base.d
+++ src/allegro5/base.d
@@ -11,12 +11,12 @@ else
 
 const int ALLEGRO_VERSION        = 5;
 const int ALLEGRO_SUB_VERSION	 = 0;
-const int ALLEGRO_WIP_VERSION	 = 7;
+const int ALLEGRO_WIP_VERSION	 = 10;
 const int ALLEGRO_RELEASE_NUMBER = 1;
 
-const char[] ALLEGRO_VERSION_STR = "5.0.7";
-const char[] ALLEGRO_DATE_STR	 = "2012";
-const int ALLEGRO_DATE         =  20120624;  /* yyyymmdd */
+const char[] ALLEGRO_VERSION_STR = "5.0.10";
+const char[] ALLEGRO_DATE_STR	 = "2013";
+const int ALLEGRO_DATE         =  20130616;  /* yyyymmdd */
 
 extern (C)
 {
